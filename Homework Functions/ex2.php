097587fe655f312dd8dcc9a201d7dcabe9470467
Delyan Kolev <delqnkolevv@gmail.com>
2016-11-30 11:32:04 +0200

    funct

--- Homework Functions/ex2.php
+++ Homework Functions/ex2.php
@@ -2,12 +2,11 @@
 // Напишете функция, която приема за параметър две числа и разпечатва по-голямото от тях;
 
 
-function bigger($number1, $number2){
-	if($number1 > $number2){
-		echo $number1;
-	}else {
-		echo $number2;
+	function bigger($number1, $number2){
+		echo ($number1 > $number2) ? $number1 : $number2;
 	}
-}
-bigger(-20,-20);
+	
+bigger(-20,20);
+
+
 ?>
